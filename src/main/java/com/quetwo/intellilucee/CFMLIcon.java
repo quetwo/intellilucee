7d3b16532e17efbb1613f5d07dee8c5ac6628671
Nick Kwiatkowski <quetwo@hostingplace.net>
2026-08-11 23:10:29 -0400

    Removed Kotlin samples, added sample icon (needs to be replaced)

---
+++ src/main/java/com/quetwo/intellilucee/CFMLIcon.java
@@ -0,0 +1,15 @@
+package com.quetwo.intellilucee;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+public final class CFMLIcon
+{
+    public static final Icon FILE = IconLoader.getIcon("/icons/img.png", CFMLIcon.class);
+
+    private  CFMLIcon()
+    {
+
+    }
+}
---
+++ src/main/java/com/quetwo/intellilucee/actions/CFMLFileCreateActions.java
@@ -0,0 +1,46 @@
+package com.quetwo.intellilucee.actions;
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction;
+import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.psi.PsiDirectory;
+import com.quetwo.intellilucee.CFMLIcon;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public final class CFMLFileCreateActions extends CreateFileFromTemplateAction implements DumbAware
+{
+
+
+    private static final @NonNls
+    @NotNull String CLASS_TEMPLATE_TAG_NAME = "CFML Class (tag)";
+    private static final @NonNls
+    @NotNull String CLASS_TEMPLATE_SCRIPT_NAME = "CFML Class (script)";
+    private static final @NonNls
+    @NotNull String PAGE_TEMPLATE_TAG_NAME = "CFML Page (tag)";
+    private static final @NonNls
+    @NotNull String PAGE_TEMPLATE_SCRIPT_NAME = "CFML Page (script)";
+
+    public CFMLFileCreateActions()
+    {
+        super("CFML File","Create CFML File", CFMLIcon.FILE);
+    }
+
+    @Override
+    protected void buildDialog(@NotNull Project project, @NotNull PsiDirectory psiDirectory, CreateFileFromTemplateDialog.@NotNull Builder builder)
+    {
+        builder.setTitle( "New CFML File" )
+                .addKind( "CFML Class (tag)", CFMLIcon.FILE, CLASS_TEMPLATE_TAG_NAME )
+                .addKind( "CFML Class (script)", CFMLIcon.FILE, CLASS_TEMPLATE_SCRIPT_NAME )
+                .addKind( "CFML Page (tag)", CFMLIcon.FILE, PAGE_TEMPLATE_TAG_NAME )
+                .addKind( "CFML Page (script)", CFMLIcon.FILE, PAGE_TEMPLATE_SCRIPT_NAME );
+    }
+
+    @Override
+    protected @NlsContexts.Command String getActionName(PsiDirectory psiDirectory, @NonNls @NotNull String s, @NonNls String s1)
+    {
+        return "Create CFML File";
+    }
+}
