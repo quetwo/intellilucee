6fcf9fb22f4dddf7a014d2dc01d43fb14e1334ab
Nick Kwiatkowski <quetwo@hostingplace.net>
2026-08-11 21:05:51 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.intellij.platform")
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        intellijIdea("2022.2.1")
+        testFramework(TestFrameworkType.Platform)
+
+        // Add plugin dependencies for compilation here:
+        bundledPlugin("com.intellij.java")
+        bundledPlugin("JavaScript")
+        bundledPlugin("com.intellij.modules.json")
+        bundledPlugin("com.intellij.properties")
+    }
+}
